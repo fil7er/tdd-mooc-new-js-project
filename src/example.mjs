239ec7e12aa8afddc6d8a428035d09e1313172b3
fil7er <fil7er@gmail.com>
2022-05-31 18:02:27 -0400

    Update test

--- src/example.mjs
+++ src/example.mjs
@@ -1,4 +1,40 @@
-export function distance(value) {
-  return value;
+import fs from 'fs';
+
+export function distance(lat1, lon1, lat2, lon2, unit) {
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+  }
+  else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+          dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit=="K") { dist = dist * 1.609344 }
+      if (unit=="N") { dist = dist * 0.8684 }
+      return dist;
+  }
 }
-f
+
+export function randomGP(n, r, a) {
+  let curt;
+  for (let i=0; i < Math.random(); i++)
+  {
+    curt[i] = Math.random() * Math.pow(Math.random(), i);
+  }
+  return curt;
+}
+
+export function findWord(){
+  fs.writeFileSync("file.txt", "random string");
+}
+
+export function resetFile() {
+  fs.truncate("file.txt");
+}
--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,9 +1,36 @@
 import { expect } from "chai";
 import { distance } from "../src/example.mjs";
+import "chai-fs";
 
 describe("Example test fixture", () => {
-  it("Calculate distance between São Paulo and North Pole in km", () => {
+  it("Calculate distance between random coordinates and the time required", () => {
+    const lat1 = getRandomInt(-90, 90);
+    const long1 = getRandomInt(-90, 90);
+    const lat2 = getRandomInt(-90, 90);
+    const long2 = getRandomInt(-90, 90);
+
+
+
+    expect(now()).to.equal(new Date().getTime());
     expect(distance).to.be.greaterThan(7290);
     expect(distance).to.be.lessThan(7310);
   });
 });
+
+describe("Get a PG of random int", () =>{
+  it("get the sequence in Geometric Progression", () => {
+    expect(randomGP(5, 3, 2)).to.equal(randomGP(Math.random(), Math.random(), Math.random()));
+});
+});
+
+
+describe("File test", () => {
+  // Never will find the right word, is randoming every time =D
+  it("find the world", () => {
+      expect(readFile("file.txt")).to.equal((Math.random() + 1).toString(36).substring(7));
+      resetFile();
+  });
+});
+
+
+
